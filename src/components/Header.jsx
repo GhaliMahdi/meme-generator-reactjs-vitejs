4bf30b1f05992cd463f264e7f95fcaa9596764dd
GhaliMahdi <ghali.mahdi99@gmail.com>
2022-12-16 19:19:09 +0100

    Updated Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
         <header className="header">
           <img src={TrollFace} alt='' className="header--image"/>
           <h2 className="header--title">Meme Generator</h2>
-          <p className="header--project">React Course - Project 3</p>
+          <p className="header--project">Learning React - State</p>
       </header>
     )
 }
